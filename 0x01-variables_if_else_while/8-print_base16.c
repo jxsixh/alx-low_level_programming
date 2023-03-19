968854f4f3dc3a2f5152e481a225bbfc883c9565
jxsixh <josiahdeshi@gmail.com>
2023-03-19 09:45:57 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int num;
 	char letter;
 		for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0')
+		putchar((num % 10) + '0');
 			for (letter = 'a'; letter <= 'z'; letter++)
 				putchar(letter);
 	putchar('\n');
