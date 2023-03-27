4cbaa1a48438b05921299f587322587431b6da33
jxsixh <josiahdeshi@gmail.com>
2023-03-27 11:34:59 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,13 +4,11 @@
 /**
  * reset_to_98 - takes a pointer and updates the value
  * @n: to check
- * Return: Always 0.
+ * Return: void
  */
 void reset_to_98(int *n)
 {
 
 	*n = 98;
-
-	return (0);
 }
 
