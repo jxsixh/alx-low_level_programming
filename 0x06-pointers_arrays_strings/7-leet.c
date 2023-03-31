3fb65e90eb4cc54f8e81344b93127e90a4c391f6
jxsixh <josiahdeshi@gmail.com>
2023-03-31 03:43:42 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string
+ * Return: char
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(str + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == str[i])
+				str[i] = b[j];
+		}
+	}
+	return (str);
+}
