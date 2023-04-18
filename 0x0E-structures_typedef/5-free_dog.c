99ea9b7daedb0cbd0ae5f71f5aaba2057331a848
jxsixh <josiahdeshi@gmail.com>
2023-04-18 10:23:55 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
+
